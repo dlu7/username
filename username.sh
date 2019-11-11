c400e856c024c4975ceed911b9f9c22dd100a97d
Debbie Lu <dlu@chapman.edu>
2019-11-11 03:06:10 +0000

    Added new version of username.sh and adds username-input

--- username.sh
+++ username.sh
@@ -1,12 +1,21 @@
 #! /bin/bash
 # username.sh
 # Debbie Lu
+
+echo "Rules for the username:"
+echo "- The only characters that can be used are"
+echo "   - lower case letters,"
+echo "   - digits, and"
+echo "   - the underscore character"
+echo "- It must start with a lower case letter"
+echo "- It must contain at least three but no more than 12 characters"
+echo ""
 echo "Enter a valid username: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[a-z]{12}$" > /dev/null 2>&1
+read username
+while echo "$username" | egrep -v "^[a-z]{3,12}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username -maximum of 12 characters only!"
-	echo "Enter a maximum of 12 characters for the valid username: "
-	read ZIP
+	echo "You must enter a valid username -- minimum of 2 characters and maximum of 12 characters only!"
+	echo "Enter a valid username: "
+	read username
 done
 echo "Thank you"
